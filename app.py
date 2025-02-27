727dca18e2c4a6fd10492ed324a8d9eb56884d80
Aman Kumar Deo <163316020+Aman-Deo123@users.noreply.github.com>
2025-02-27 22:09:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,8 +55,8 @@ import streamlit as st
 import requests
 
 # Fetch API keys from Streamlit Secrets
-GOOGLE_API_KEY = ["AIzaS****"]
-SEARCH_ENGINE_ID = ["72***"]
+GOOGLE_API_KEY = "AIzaSyDxGT6GCyft5sHeCbV3qIe487tQ2lou_Bo"
+SEARCH_ENGINE_ID = "7220b7d5a05ef48e0"
  
 
 def google_search(query):
